4f02af0ab134a65dd629891effc4002b9cef6599
Ashish <ashishkrgupta@hotmail.com>
2020-01-04 00:46:29 +0530

    Updates

---
+++ precache-manifest.c0d8865f8098f8f1f6313293d189f5f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0f0fc067aa56ed29f164c9b06fdc98b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbb347d129ef50925d80",
+    "url": "/static/css/main.deb85818.chunk.css"
+  },
+  {
+    "revision": "d5cd1a20f4f2a03f1c4b",
+    "url": "/static/js/2.6064655a.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.6064655a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bbb347d129ef50925d80",
+    "url": "/static/js/main.faae9a3e.chunk.js"
+  },
+  {
+    "revision": "84d71d569343dbd92bf1",
+    "url": "/static/js/runtime-main.39004606.js"
+  }
+]);
