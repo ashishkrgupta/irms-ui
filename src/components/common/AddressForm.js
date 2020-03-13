374c331cb46f9e431f1d1a1b8a860b6fc3ec9403
Ashish <ashishkrgupta@hotmail.com>
2020-03-13 11:37:40 +0530

    incorporated some of the review comments

--- src/components/common/AddressForm.js
+++ src/components/common/AddressForm.js
@@ -3,6 +3,8 @@ import Divider from '@material-ui/core/Divider'
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import {countries} from './CountryStateData';
 
 export default class AddressForm extends Component {
 
@@ -13,16 +15,34 @@ export default class AddressForm extends Component {
       line2:"",
       city:"",
       state:"",
-      country:"",
+      country:"India",
       pin:""
-    }
-  };
+    },
+    states: [],
+  }; 
 
   onChangeHandle = (value, field) => {
     let addr = {...this.state.address};
     addr[field] = value;
     this.setState({address: addr});
     this.props.onChange(this.state.address);
+  }
+
+  onCountryChange = (event, value) => {
+    let address = {...this.state.address};
+    address.country = value;
+    address.state = "";
+    let states = countries.find(obj => obj.country === value);
+    states = states ? states.states : [];
+    this.setState({address, states });
+    this.props.onChange(address);
+  }
+
+  onStateChange = (event, value) => {
+    let address = {...this.state.address};
+    address.state = value;
+    this.setState({address });
+    this.props.onChange(address);
   }
 
   componentDidUpdate() {
@@ -48,15 +68,34 @@ export default class AddressForm extends Component {
                 onChange={e => this.onChangeHandle(e.target.value, "city")}/>
           </Grid>
           <Grid item xs={3}>
-            <TextField className="width100percent" label="State/Provience" 
-                onChange={e => this.onChangeHandle(e.target.value, "state")}/>
+            <Autocomplete className="width100percent"
+              options={countries}
+              getOptionLabel={option => option.country} 
+              style={{ width: "100%" }}
+              defaultValue={"India"}
+              inputValue={this.state.address.country}//"India"
+              onInputChange={this.onCountryChange}
+              renderInput={params => <TextField style={{ width: "100%" }} {...params} label="Country" />}
+            />
+            {/*<TextField className="width100percent" label="Country" 
+                onChange={e => this.onChangeHandle(e.target.value, "country")}/>*/}
+          </Grid>
+
+          <Grid item xs={3}>
+            <Autocomplete className="width100percent"
+              options={this.state.states}
+              //getOptionLabel={option => option.country} 
+              style={{ width: "100%" }}
+              inputValue={this.state.address.state}//"India"
+              onInputChange={this.onStateChange}
+              renderInput={params => <TextField style={{ width: "100%" }} {...params} label="State/Provience" />}
+            />
+            {/*<TextField className="width100percent" label="State/Provience" 
+                onChange={e => this.onChangeHandle(e.target.value, "state")}/>*/}
+            
           </Grid>
           <Grid item xs={3}>
-            <TextField className="width100percent" label="Country" 
-                onChange={e => this.onChangeHandle(e.target.value, "country")}/>
-          </Grid>
-          <Grid item xs={3}>
-            <TextField className="width100percent" label="Pin Code" type="number"
+            <TextField className="width100percent" label="Pin Code"
                 onChange={e => this.onChangeHandle(e.target.value, "pin")}/>
           </Grid>
         </Grid>
