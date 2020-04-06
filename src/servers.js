77ddeff0f24922018a5ea9d6724111b1215bbc4b
Ashish <ashishkrgupta@hotmail.com>
2020-04-07 01:18:29 +0530

    uncommented dev urls

--- src/servers.js
+++ src/servers.js
@@ -4,6 +4,10 @@ export const IRMS_SERVICE = axios.create({
   baseURL: "https://cors-anywhere.herokuapp.com/https://irms-service.herokuapp.com/irms-service",
 });
 
+//export const IRMS_SERVICE = axios.create({
+//  baseURL: "http://localhost:8080/irms-service",
+//});
+
 IRMS_SERVICE.defaults.headers = {
   'Content-Type': 'application/json'
 }
